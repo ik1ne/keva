67d48c12f47f5cdeb2564e890d4b21d5058615c3
ik1ne <ik1ne@naver.com>
2025-12-27 00:27:03 +0900

    run cargo fmt

--- search/src/engine/mod.rs
+++ search/src/engine/mod.rs
@@ -113,8 +113,10 @@ impl SearchEngine {
             Normalization::Never
         };
 
-        self.active.set_pattern(pattern, case_matching, normalization);
-        self.trash.set_pattern(pattern, case_matching, normalization);
+        self.active
+            .set_pattern(pattern, case_matching, normalization);
+        self.trash
+            .set_pattern(pattern, case_matching, normalization);
 
         self.active_finished = false;
         self.trashed_finished = false;
